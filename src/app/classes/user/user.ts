78a503b593ddcf75815f4f1adeac76db536072f0
murru simone <simone.murru@winwinit.tech>
2022-04-29 16:59:32 +0200

    First version of Form (working).

---
+++ src/app/classes/user/user.ts
@@ -0,0 +1,8 @@
+export class User {
+  constructor(
+    public name: string,
+    public email: string,
+    public gender: string,
+    public status: string
+  ) {}
+}
---
+++ src/app/components/user/user-registration/user-registration.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/classes/user/user';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { ENDPOINTS } from 'src/app/constants';
+
+@Component({
+  selector: 'app-user-registration',
+  templateUrl: './user-registration.component.html',
+  styleUrls: ['./user-registration.component.css'],
+})
+export class UserRegistrationComponent implements OnInit {
+  genderList = [
+    { label: 'Male', value: 'male' },
+    { label: 'Female', value: 'female' },
+  ];
+  statusList = [
+    { label: 'Active', value: 'active' },
+    { label: 'Inactive', value: 'inactive' },
+  ];
+
+  constructor(public http: HttpClient) {}
+
+  userForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.pattern("^s*([A-Za-z]{1,}([.,] |[-']| ))+[A-Za-z]+.?s*$"),
+      ]),
+      gender: new FormControl(this.genderList[0].value),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      status: new FormControl(this.statusList[0].value),
+    });
+  }
+
+  get name() {
+    return this.userForm.get('name');
+  }
+
+  get email() {
+    return this.userForm.get('email');
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.userForm.value);
+    this.http
+      .post(
+        environment.baseUrl + ENDPOINTS.USER_ENDPOINT,
+        this.userForm.value,
+        {
+          headers: { Authorization: 'Bearer ' + environment.token },
+        }
+      )
+      .subscribe();
+  }
+}
